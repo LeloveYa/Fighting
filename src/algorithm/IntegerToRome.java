fc984645656864bb940f35a1b1aa2e43e30bc483
LE <zhaole312@163.com>
2017-01-19 00:14:20 +0100

    1.19

---
+++ src/algorithm/IntegerToRome.java
@@ -0,0 +1,20 @@
+package algorithm;
+
+/*
+ Given an integer, convert it to a roman numeral.
+Input is guaranteed to be within the range from 1 to 3999.
+ */
+
+//I=1 V=5 X=10 L=50 C=100 D=500 M=1000
+public class IntegerToRome{
+	static String IntegerToRome(int num){
+ String M[] = {"", "M", "MM", "MMM"};
+ String C[] = {"", "C", "CC", "CCC", "CD", "D", "DC", "DCC", "DCCC", "CM"};
+ String X[] = {"", "X", "XX", "XXX", "XL", "L", "LX", "LXX", "LXXX", "XC"};
+ String I[] = {"", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX"};
+		    return M[num/1000] + C[(num%1000)/100] + X[(num%100)/10] + I[num%10];
+	}
+	public static void main(String[]args){
+		System.out.println(IntegerToRome(666));
+	}
+}
